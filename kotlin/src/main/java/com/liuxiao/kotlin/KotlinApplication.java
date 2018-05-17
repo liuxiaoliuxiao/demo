fe23a3340df0a24654b48fcb9436157438ebbf32
liuxiaoliuxiao <752269601@qq.com>
2018-05-17 14:53:00 +0800

    Initial commit

---
+++ kotlin/src/main/java/com/liuxiao/kotlin/KotlinApplication.java
@@ -0,0 +1,12 @@
+package com.liuxiao.kotlin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotlinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KotlinApplication.class, args);
+	}
+}
